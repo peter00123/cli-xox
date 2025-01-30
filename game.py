d0334f9a2a89d5afcd1bece6883f51bac50abaf2
peter00123 <basilmathew833@gmail.com>
2025-01-30 19:08:50 +0530

    completed

--- game.py
+++ game.py
@@ -1,54 +1,101 @@
-def iteration(matrix,row,col,count):
-    matrix[1][2]="x"
-        
 
+
+def end(winr):
+    print(f"winner is {winr}")
+    exit()
+
+def inserter(matrix,row,col,count,used,player,size):
+
+
+    for row in matrix:
+        print(row)
     if(count==1):
         exit()
+
+    if(player=="x"):
+        insert = str(input("enter where do u want to insert "+player+"    "))
+
+    else:
+        insert = str(input("enter where do u want to insert "+player+"    "))
+
+
+
+    a = int(insert[0] )
+    b = int(insert[1])
+    val = matrix[a-1][b-1]
+    if val in used:
+        print("already used")
+        inserter(matrix,row,col,count,used,player,size)
+    else:
+
+        used.append(val)
+        matrix[a-1][b-1]=player
+
+
+        count+1
+        if(player=="x"):
+            player = "o"
+        else:
+            player = "x"
     
-    count+1
-    check_diagonals(matrix,row,col,count)
+        print(used)
+        print(val)
+
+        
+        checker(matrix,row,col,count,used,player,size)
 
 
 
-def check_diagonals(matrix,row,col,count):
+def checker(matrix,row,col,count,used,player,size):
     # Check rows
     for row in matrix:
         if len(set(row)) == 1 and row[0] != " ":
             return f"Winner: {row[0]} in row"
+            winr = row[0]
+            end(winr)
+            
     
     # Check columns
-    for col in range(3):
-        column = [matrix[row][col] for row in range(3)]
+    for col in range(size):
+        column = [matrix[row][col] for row in range(size)]
         if len(set(column)) == 1 and column[0] != " ":
             print(f"Winner: {column[0]} in column")
+            winr = column[0]
+            end(winr)
+            
     
     # Check main diagonal
-    main_diag = [matrix[i][i] for i in range(3)]
+    main_diag = [matrix[i][i] for i in range(size)]
     if len(set(main_diag)) == 1 and main_diag[0] != " ":
         print(f"Winner: {main_diag[0]} in main diagonal")
+        winr = main_diag[0]
+        end(winr)
+            
     
     # Check anti diagonal
-    anti_diag = [matrix[i][2-i] for i in range(3)]
+    anti_diag = [matrix[i][2-i] for i in range(size)]
     if len(set(anti_diag)) == 1 and anti_diag[0] != " ":
         print(f"Winner: {anti_diag[0]} in anti diagonal")
+        winr = anti_diag[0]
+        end(winr)
 
-    for row in matrix:
-        print(row)
+
     
-    iteration(matrix,row,col,count)
+    inserter(matrix,row,col,count,used,player,size)
 
-def start(difficualti):
+def start(size):
+    size-1
     print("you are X ")
     print("computer is O")
-    print("enter the number, where u want to place the X")
+    print("enter the number, where u want to place the X or O")
     
-    print(difficualti)
+    print(size)
 
 
     matrix = []
-    for i in range(difficualti):  # Outer loop for rows
+    for i in range(size):  # Outer loop for rows
         row = []
-        for j in range(difficualti):  # Inner loop for columns
+        for j in range(size):  # Inner loop for columns
             row.append(str((i+1)*10 + (j+1)))  # Compute value
         matrix.append(row)  # Append row to matrix
 
@@ -56,7 +103,7 @@ def start(difficualti):
 
     """matrix = [[(i+1)*10 + (j+1) for j in range(3)] for i in range(3)]"""
     
-    
+    used = []
 
     """matrix = [
         [11,12,13],
@@ -65,7 +112,8 @@ def start(difficualti):
     ]"""
     col = row
     count = 0
-    check_diagonals(matrix,row,col,count)
+    player = "x"
+    inserter(matrix,row,col,count,used,player,size)
     
 
 
--- main.py
+++ main.py
@@ -5,43 +5,43 @@ from game import start
 while (exit != True):
     for i in range(3):
         print(" ")
-    difficualti = int(input("choose the difficulti level from 0 to 3, and press enter  enter _e_ for exiting "))
+    size = int(input("choose the number of box "))
     for i in range(3):
         print("  ")
 
-    if(difficualti == 0):
+    if(size == 0):
         print("enter _e_ for exiting")
         print("you have chosen the easy level")
         for i in range(3):
             print(" ")
         exit = True
-    elif(difficualti == 1):
+    elif(size == 1):
         print("enter _e_ for exiting")
         print("you have chosen the medium level")
         for i in range(3):
             print(" ")
         exit = True
-    elif(difficualti == 2):
+    elif(size == 2):
         print("enter _e_ for exiting")
         print("you have chosen the hard level")
         for i in range(3):
             print(" ")
         exit = True
-    elif(difficualti == 3):
+    elif(size == 3):
         print("enter _e_ for exiting")
         print("you have chosen the hard level")
         for i in range(3):
             print(" ")
         exit = True
-    elif(difficualti == "e"):
+    elif(size == "e"):
         print("you have exited the game")
         for i in range(3):
             print(" ")
         exit = True
         exit()
 
-print(difficualti)
+print(size)
 
 
 
-start(difficualti)
+start(size)
